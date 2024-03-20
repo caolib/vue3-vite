be829bcdc9b3e3ebd45c0e7a99a95e0a97451433
caolibin <1265501579@qq.com>
2024-03-20 22:53:50 +0800

    2024年3月20日22:53:24

--- src/main.js
+++ src/main.js
@@ -1,31 +1,27 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import {createApp} from 'vue'
-import App from './App.vue'
-import router from '@/router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "@/router";
 import ElementPlus from "element-plus";
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-import {createPersistedState} from "pinia-persistedstate-plugin";
+import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/dark/css-vars.css";
+import { createPersistedState } from "pinia-persistedstate-plugin";
 
-import '@/css/main.css';
-import {createPinia} from "pinia";
-
-
+import "@/css/main.css";
+import { createPinia } from "pinia";
 
 // 导入element-plus中文语言
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-const app = createApp(App)
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+const app = createApp(App);
 
 // 使用pinia和persist保存状态并持久化
 const pinia = createPinia();
 const persist = createPersistedState();
 pinia.use(persist);
 
-app.use(ElementPlus, {locale: zhCn,});
+app.use(ElementPlus, { locale: zhCn });
 app.use(router);
 app.use(pinia);
 
-app.mount('#app')
-
-
+app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,40 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    //代理http请求，解决跨域问题
-    server: {
-        // host: '192.168.0.66',
-        host: 'localhost',
-        port: 5173,
-        proxy: {
+  },
+  //代理http请求，解决跨域问题
+  server: {
+    // host: '192.168.0.66',
+    // 前端地址
+    host: "localhost",
+    port: 5173,
+    proxy: {
             '/api': { //匹配请求路径中含有 /api 的请求
                 // target: 'http://47.115.226.138:8080', //后端服务地址
                 target: 'http://localhost:8080', //后端服务地址
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '') //去除路径中的/api，还原请求路径
-            }
-        }
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), //去除路径中的/api，还原请求路径
+      },
     },
-})
+  },
+});
